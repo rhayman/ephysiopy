d0f8ae8a46ed7b68adeae8e50b16b39f224ce9e8
Robin Hayman <r.hayman@ucl.ac.uk>
2020-04-01 15:14:19 +0100

    updating docs

--- docs/conf.py
+++ docs/conf.py
@@ -63,3 +63,7 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# To avoid getting contents.rst not found errors on readthedocs use the index.html
+# file as the master document
+master_doc = 'index'
