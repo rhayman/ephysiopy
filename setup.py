8b5e9de4daf646caf6ed114da357299618f7944d
Robin Hayman <r.hayman@ucl.ac.uk>
2021-05-19 18:24:44 +0100

    trying to fix documentation build failure

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 import setuptools
-from ephysiopy import __about__
+import ephysiopy.__about__
 
 with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name=__about__.__project__,
-    version=__about__.__version__,
-    author=__about__.__author__,
+    name=ephysiopy.__about__.__project__,
+    version=ephysiopy.__about__.__version__,
+    author=ephysiopy.__about__.__author__,
     author_email="robin.hayman@gmail.com",
     description="Analysis of electrophysiology data",
     long_description=long_description,
@@ -15,7 +15,7 @@ setuptools.setup(
     url="https://github.com/rhayman/ephysiopy",
     packages=setuptools.find_packages(),
     include_package_data=True,
-    package_data={"": ["*.pkl", "*.txt"]},
+    package_data={"": ["*.txt"]},
     install_requires=[
         "numpy",
         "scipy",
