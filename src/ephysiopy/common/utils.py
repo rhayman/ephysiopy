c878ebf938695959789f22d4691980595ebf606f
Robin Hayman <robin.hayman@gmail.com>
2026-07-31 16:05:04 +0100

    version bump and removed mahotas dependency

--- src/ephysiopy/common/utils.py
+++ src/ephysiopy/common/utils.py
@@ -1424,7 +1424,7 @@ def bwperim(bw, n=4):
     """
 
     if n not in (4, 8):
-        raise ValueError("mahotas.bwperim: n must be 4 or 8")
+        raise ValueError("n must be 4 or 8")
     rows, cols = bw.shape
 
     # Translate image by one pixel in all directions
