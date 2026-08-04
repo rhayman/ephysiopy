faulty spike masking fixed

src/ephysiopy/common/ephys_generic.py
@@ -746,7 +746,7 @@ class PosCalcsGeneric(object):
         new_y = signal.resample_poly(xy[1, :], upsample_rate / denom, 30 / denom)
         return np.array([new_x, new_y])
 
-    def apply_mask(self, mask: np.ndarray):
+    def apply_mask(self, mask: np.ndarray | bool | None) -> None:
         """
         Applies a mask to the position data
 
@@ -763,6 +763,8 @@ class PosCalcsGeneric(object):
         i.e. [(start1, end1), (start2, end2)]
         everything inside of these times is masked
         """
+        if mask is None:
+            mask = False
         attrs = ["x", "y", "time", "dir", "speed"]
         for attr in attrs:
             if hasattr(self, attr):

src/ephysiopy/openephys2py/KiloSort.py
@@ -310,7 +310,7 @@ class KiloSortSession(object):
             return np.array([], dtype=int)
         return np.nonzero(np.isin(clusters, cluster_ids))[0]
 
-    def apply_mask(self, mask: np.ma.MaskedArray | bool, **kwargs):
+    def apply_mask(self, mask: np.ma.MaskedArray | bool | None, **kwargs):
         """
         Apply a mask to the data.
 
@@ -325,7 +325,7 @@ class KiloSortSession(object):
         # the xy times might not be equally sampled (indeed likely arent)
         # mask might be a single bool (False) to remove the masking
         # or an array of mask values to apply to the spike times and clusters
-        if isinstance(mask, np.ma.MaskedArray):
+        if np.any(mask):
             xy_ts = kwargs.get("xy_ts", None)  # in seconds to 2 decimal places
             # extract the masked times
             xy_ts = xy_ts.data[~xy_ts.mask]
@@ -338,18 +338,12 @@ class KiloSortSession(object):
 
             mask = np.ma.isin(indices_seconds, xy_ts, assume_unique=True)
 
-        if isinstance(self.spike_times, np.ma.MaskedArray):
-            self.spike_times.mask = mask
-        else:
-            self.spike_times = np.ma.MaskedArray(self.spike_times, mask)
-        if isinstance(self.spike_clusters, np.ma.MaskedArray):
-            self.spike_clusters.mask = mask
-        else:
-            self.spike_clusters = np.ma.MaskedArray(self.spike_clusters, mask)
-        if isinstance(self.amplitudes, np.ma.MaskedArray):
-            self.amplitudes.mask = mask
-        else:
-            self.amplitudes = np.ma.MaskedArray(self.amplitudes, mask)
+        elif mask is None:
+            mask = False
+
+        self.spike_times.mask = mask
+        self.spike_clusters.mask = mask
+        self.amplitudes.mask = mask
 
     def _load_channel_positions(self):
         """